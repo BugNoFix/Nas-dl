441c958fdcbf0d78d985e823e25bc9346636e5af
bugnofix <1marcothegame@gmail.com>
2019-11-24 16:57:26 +0100

    update anime unity

--- extractor/AnimeUnity.py
+++ extractor/AnimeUnity.py
@@ -3,21 +3,22 @@
 
 # Copyright (c) 2019 bugnofix. Under MIT License.
 
-import requests, os, re, json, urllib.parse
+import requests, os, re, json, urllib.parse, cfscrape
 from utilies import RemoveSpecialCharacter
 
 # Load configs
 CONFIG = json.load(open('setting.json'))
 
-AnimeUnity_VALID = r'https:\/\/animeunity\.it\/.*'
-base_url = 'https://animeunity.it/'
+AnimeUnity_VALID = r'https:\/\/www.animeunity\.it\/.*'
+base_url = 'https://www.animeunity.it/'
 
 def AnimeUnity(URL):
 	# Varibili
 	a = 0
 
 	# Cerco i dati
-	page = requests.get(URL).text
+	sess = cfscrape.create_scraper(requests.session())
+	page = (sess.get(URL).text)
 	name = re.search(r'<p>\s*<b>TITOLO: <\/b>(.*)											<\/p>', str(page), re.IGNORECASE).group(1)
 	ep = re.findall(r'<div class="ep-box col-lg-1 col-sm-1" style="width:19%">\s*<a href="(anime\.php\?id=.*)" ', str(page), re.IGNORECASE)
 	
@@ -25,7 +26,7 @@ def AnimeUnity(URL):
 	for link in ep:
 		a = a + 1
 		url = 'https://animeunity.it/'+ link
-		page = requests.get(url).text
+		page = sess.get(url).text
 		video = re.search(r'<source src="(.*)" type="video\/.*">\s*<\/video>', str(page), re.IGNORECASE).group(1)
 		if a < 10:
 			finalname = name + ' ep 0' +  str(a)
@@ -41,7 +42,8 @@ def AnimeUnitySearcher(anime):
 	search_url = 'https://animeunity.it/anime.php?c=archive'
 
 	# Cerco l'anime nel sito
-	page = requests.post(search_url, headers)
+	sess = cfscrape.create_scraper(requests.session())
+	page = sess.post(search_url, headers)
 	link_anime = re.findall(r'<a href="(.*)">\s*<img class="card-img archive-card-img" src=".*" alt="">\s*<\/a>\s*<\/div>\s*<\/div>\s*<div class="col-md-7 col-sm-7 archive-col" style="padding-left:0px">\s*<div class="card-block">\s*<br>\s*<h6 class="card-title"><b>(.*)<\/b><\/h6>', str(page.text), re.IGNORECASE)
 	
 	# Separo il nome dell'anime
